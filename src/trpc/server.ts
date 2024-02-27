b485500c49071c5b94691267a9389aedac4ab428
Vitor Windberg <vitorwlima13@gmail.com>
2024-02-27 11:54:40 -0300

    chore: fix build issues

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -14,6 +14,7 @@ import { cache } from "react";
 import { appRouter, type AppRouter } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
 import { transformer } from "./shared";
+import { type NextRequest } from "next/server";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
@@ -24,7 +25,9 @@ const createContext = cache(() => {
   heads.set("x-trpc-source", "rsc");
 
   return createTRPCContext({
-    headers: heads,
+    req: {
+      headers: heads,
+    } as NextRequest
   });
 });
 
